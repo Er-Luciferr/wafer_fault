3124bddc7e37411a9106493738671e08c6df4bfa
Er-Luciferr <mohitsingh2@yahoo.com>
2025-11-02 16:34:16 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mohit Singh",
+    author_email="mohitsingh2@yahoo.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+#Code is ready for deployment
+#attempt 6
